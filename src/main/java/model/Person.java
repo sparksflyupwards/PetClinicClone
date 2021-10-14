88009ccfa4e535ce35a15630c0ca44ff74aab76d
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-10-14 03:52:46 -0400

    Created Pet Clinic POJOs, Closes #2

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Person {
+
+    private String lastName;
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String newFirstName) {
+        this.firstName = newFirstName;
+    }
+
+
+
+}
