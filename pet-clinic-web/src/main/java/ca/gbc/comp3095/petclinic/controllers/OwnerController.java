37c2b8a19d23397220ca926cecac9931bd7ca683
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-11-04 14:42:52 -0400

    finished index pages with data loaded using the appropriate services Closes #7 and Closes #8

--- pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package ca.gbc.comp3095.petclinic.controllers;
 
+import ca.gbc.comp3095.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/" , "/index"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll() );
         return "owners/index";
     }
+
+
 }
--- pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package ca.gbc.comp3095.petclinic.controllers;
 
+import ca.gbc.comp3095.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+    private final VetService vetService;
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"", "/" , "/index"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
