cb231b9baf10dea588c0518da758e1b14d32a1ba
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-12-14 22:31:58 -0500

    Add Contact Information Properties to Owner closes #24

--- pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/model/Owner.java
@@ -7,6 +7,33 @@ public class Owner extends Person{
 
     private Set<Pet> pets = new HashSet<Pet>();
 
+    private String address;
+    private String city, telephone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
     public Set<Pet> getPets() {
         return pets;
     }
@@ -14,4 +41,5 @@ public class Owner extends Person{
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+
 }
