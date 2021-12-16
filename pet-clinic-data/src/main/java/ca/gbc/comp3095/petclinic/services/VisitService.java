389814dc7948cc934fb77c03a99ed08a2a3a0c53
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-12-15 22:39:56 -0500

    Create Visit Map Service

---
+++ pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package ca.gbc.comp3095.petclinic.services;
+
+import ca.gbc.comp3095.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package ca.gbc.comp3095.petclinic.services.map;
+
+import ca.gbc.comp3095.petclinic.model.Visit;
+import ca.gbc.comp3095.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile({"default","map"})
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+}
